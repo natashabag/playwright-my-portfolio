7707c8f4cdf11f7930c5077c77cbad3c26d48cd2
Natasha Bagramian <natalia.bagramian@gmail.com>
2024-06-05 14:50:18 -0700

    orginise POM

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,15 +1,9 @@
 import { test, expect } from "@playwright/test";
-import {
-  navigationSelectors,
-  projectSelectors,
-  socialMediaSelectors,
-  arrowSelectors,
-  footerSelectors,
-} from "./selectors.ts";
+import { selectors, urls, openURL } from "../pages/porfolio-page.ts";
 
 test.describe("Navigation Links", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://natashabag.github.io/");
+    await openURL(page);
   });
 
   test("should display correct title", async ({ page }) => {
@@ -17,170 +11,146 @@ test.describe("Navigation Links", () => {
   });
 
   test("should redirect to about me section", async ({ page }) => {
-    await page.click(navigationSelectors.aboutButtonTop);
-    await expect(page.locator('//*[@id="about"]/h1')).toBeVisible();
+    await page.click(selectors.aboutButtonTop);
+    await expect(page.locator(selectors.aboutHeader)).toBeVisible();
   });
 
   test("should redirect to experience section", async ({ page }) => {
-    await page.click(navigationSelectors.experienceButtonTop);
-    await expect(page.locator('//*[@id="experience"]/h1')).toBeVisible();
+    await page.click(selectors.experienceButtonTop);
+    await expect(page.locator(selectors.experienceHeader)).toBeVisible();
   });
 
   test("should redirect to projects section", async ({ page }) => {
-    await page.click(navigationSelectors.projectsButtonTop);
-    await expect(page.locator('//*[@id="projects"]/h1')).toBeVisible();
+    await page.click(selectors.projectsButtonTop);
+    await expect(page.locator(selectors.projectsHeader)).toBeVisible();
   });
 
   test("should redirect to contact me section", async ({ page }) => {
-    await page.click(navigationSelectors.contactMeButtonTop);
-    await expect(page.locator('//*[@id="contact"]/h1')).toBeVisible();
+    await page.click(selectors.contactMeButtonTop);
+    await expect(page.locator(selectors.contactHeader)).toBeVisible();
   });
 });
 
 test.describe("Button Tests", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://natashabag.github.io/");
+    await openURL(page);
   });
 
   test("should open resume pdf file", async ({ page }) => {
     const [resumePage] = await Promise.all([
       page.waitForEvent("popup"),
-      page.click("text=Download Resume"),
+      page.click(selectors.downloadResumeButton),
     ]);
     await resumePage.waitForLoadState();
-    expect(resumePage.url()).toBe(
-      "https://natashabag.github.io/assets/Natalia_Bagramian_QA_Engineer.pdf"
-    );
+    expect(resumePage.url()).toBe(urls.resumeURL);
   });
 
   test("should open github page", async ({ page }) => {
     const [gitHubPage] = await Promise.all([
       page.waitForEvent("popup"),
-      page.click("text=Visit GitHub"),
+      page.click(selectors.visitGitHubButton),
     ]);
     await gitHubPage.waitForLoadState();
-    expect(gitHubPage.url()).toBe("https://github.com/natashabag");
+    expect(gitHubPage.url()).toBe(urls.gitHubURL);
   });
-  test("should open project 1 github page", async ({ page }) => {
-    const [projectOneGitHubPage] = await Promise.all([
+  test("should open project 1 page", async ({ page }) => {
+    const [projectOnePage] = await Promise.all([
       page.waitForEvent("popup"),
-      page.locator('//*[@id="projects"]/div/div/div[1]/div[2]/button').click(),
+      page.locator(selectors.projectOneButton).click(),
     ]);
-    await projectOneGitHubPage.waitForLoadState();
-    expect(projectOneGitHubPage.url()).toBe(
-      "https://github.com/natashabag/Swag-Labs"
-    );
+    await projectOnePage.waitForLoadState();
+    expect(projectOnePage.url()).toBe(urls.projectOneURL);
   });
   test("should open project 2 github page", async ({ page }) => {
-    const [projectTwoGitHubPage] = await Promise.all([
+    const [projectTwoPage] = await Promise.all([
       page.waitForEvent("popup"),
-      page.locator('//*[@id="projects"]/div/div/div[2]/div[2]/button').click(),
+      page.locator(selectors.projectTwoButton).click(),
     ]);
-    await projectTwoGitHubPage.waitForLoadState();
-    expect(projectTwoGitHubPage.url()).toBe(
-      "https://github.com/natashabag/Luma_Selenium_Python"
-    );
+    await projectTwoPage.waitForLoadState();
+    expect(projectTwoPage.url()).toBe(urls.projectTwoURL);
   });
   test("should open project 3 github page", async ({ page }) => {
-    const [projectThreeGitHubPage] = await Promise.all([
+    const [projectThreePage] = await Promise.all([
       page.waitForEvent("popup"),
-      page.locator('//*[@id="projects"]/div/div/div[3]/div[2]/button').click(),
+      page.locator(selectors.projectThreeButton).click(),
     ]);
-    await projectThreeGitHubPage.waitForLoadState();
-    expect(projectThreeGitHubPage.url()).toBe(
-      "https://github.com/natashabag/Urban-Routes-Web-App"
-    );
+    await projectThreePage.waitForLoadState();
+    expect(projectThreePage.url()).toBe(urls.projectThreeURL);
   });
   test("should open LinkedIn page", async ({ page }) => {
     const [linkedInPage] = await Promise.all([
       page.waitForEvent("popup"),
-      page.locator('//*[@id="contact"]/div/div[2]/p/a').click(),
+      page.locator(selectors.linkedInButton).click(),
     ]);
     await linkedInPage.waitForLoadState();
-    expect(linkedInPage.url()).toBe(
-      "https://www.linkedin.com/in/natalia-bagramian"
-    );
+    expect(linkedInPage.url()).toBe(urls.linkedInURL);
   });
 });
 
 test.describe("Arrow Tests", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://natashabag.github.io/");
+    await openURL(page);
   });
 
   test("should redirect to about me section / arrow", async ({ page }) => {
-    const firstArrow = page.locator('//*[@id="profile"]/img');
-    await firstArrow.click();
-    await expect(page.locator('//*[@id="about"]/h1')).toBeVisible();
+    await page.click(selectors.firstArrow);
+    await expect(page.locator(selectors.aboutHeader)).toBeVisible();
   });
 
   test("should redirect to experience section / arrow", async ({ page }) => {
-    const secondArrow = page.locator('//*[@id="about"]/img');
-    await secondArrow.click();
-    await expect(page.locator('//*[@id="experience"]/h1')).toBeVisible();
+    await page.click(selectors.secondArrow);
+    await expect(page.locator(selectors.experienceHeader)).toBeVisible();
   });
 
   test("should redirect to projects section / arrow", async ({ page }) => {
-    const thirdArrow = page.locator('//*[@id="experience"]/img');
-    await thirdArrow.click();
-    await expect(page.locator('//*[@id="projects"]/h1')).toBeVisible();
+    await page.click(selectors.thirdArrow);
+    await expect(page.locator(selectors.projectsHeader)).toBeVisible();
   });
 
   test("should redirect to manual projects section / arrow", async ({
     page,
   }) => {
-    const fourthArrow = page.locator('//*[@id="projects"]/img');
-    await fourthArrow.click();
-    await expect(page.locator('//*[@id="manual-projects"]/h1')).toBeVisible();
+    await page.click(selectors.fourthArrow);
+    await expect(page.locator(selectors.manualProjectsHeader)).toBeVisible();
   });
 
   test("should redirect to contact me section / arrow", async ({ page }) => {
-    const fifthArrow = page.locator('//*[@id="manual-projects"]/img');
-    await fifthArrow.click();
-    await expect(page.locator('//*[@id="contact"]/h1')).toBeVisible();
+    await page.click(selectors.fifthArrow);
+    await expect(page.locator(selectors.contactHeader)).toBeVisible();
   });
 
   test("should redirect to the top of the screen / arrow", async ({ page }) => {
-    const buttomArrow = page.locator('//*[@id="contact"]/img');
-    await buttomArrow.click();
-    await expect(page.locator('//*[@id="profile"]/h1')).toBeVisible();
+    await page.click(selectors.buttomArrow);
+    await expect(page.locator(selectors.profileHeader)).toBeVisible();
   });
 });
 
 test.describe("Footer Tests", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://natashabag.github.io/");
+    await openURL(page);
   });
 
   test("should redirect to about me section from footer", async ({ page }) => {
-    const aboutButton = page.getByRole("link", { name: "About me" }).nth(1);
-    await aboutButton.click();
-    await expect(page.locator('//*[@id="about"]/h1')).toBeVisible();
+    await page.click(selectors.aboutButtonFooter);
+    await expect(page.locator(selectors.aboutHeader)).toBeVisible();
   });
 
   test("should redirect to experience section from footer", async ({
     page,
   }) => {
-    const experienceButton = page
-      .getByRole("link", { name: "Experience" })
-      .nth(1);
-    await experienceButton.click();
-    await expect(page.locator('//*[@id="experience"]/h1')).toBeVisible();
+    await page.click(selectors.experienceButtonFooter);
+    await expect(page.locator(selectors.experienceHeader)).toBeVisible();
   });
 
   test("should redirect to projects section from footer", async ({ page }) => {
-    const projectsButton = page.getByRole("link", { name: "Projects" }).nth(1);
-    await projectsButton.click();
-    await expect(page.locator('//*[@id="projects"]/h1')).toBeVisible();
+    await page.click(selectors.projectsButtonFooter);
+    await expect(page.locator(selectors.projectsHeader)).toBeVisible();
   });
 
   test("should redirect to contact me section from footer", async ({
     page,
   }) => {
-    const contactMeButton = page
-      .getByRole("link", { name: "Contact me" })
-      .nth(1);
-    await contactMeButton.click();
-    await expect(page.locator('//*[@id="contact"]/h1')).toBeVisible();
+    await page.click(selectors.contactMeButtonFooter);
+    await expect(page.locator(selectors.contactHeader)).toBeVisible();
   });
 });
